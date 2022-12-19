de66f36309a7627f80234f5dc819e0da44bf2e61
Juan0ndiviela <113891041+Juan0ndiviela@users.noreply.github.com>
2022-12-19 10:28:31 +0100

    Add files via upload

---
+++ 19 Diciembre 2022.cpp
@@ -0,0 +1,19 @@
+//Mi primer programa en C++ KEKW
+#include<iostream>
+using namespace std;
+int main(){
+	//Definimos tres variables
+	int edad;
+	cout<<"Introduce tu edad: ";
+	cin>> edad;
+	if(edad>=18){
+		cout<<"Eres mayor de edad";
+	}
+	else{
+		cout<<"Eres menor de edad";
+	}	
+	return 0;
+	
+	
+	
+}
